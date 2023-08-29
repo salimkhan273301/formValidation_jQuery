1bb29af689b3bb8313f60266948b08ef9fd97f6a
salimkhan273301 <ajadi273301@gmail.com>
2023-08-29 21:59:26 +0530

    dynamic form

--- registrationForm/index.js
+++ registrationForm/index.js
@@ -1,5 +1,5 @@
-function validateCountryCode(inputId) {
-  const inputElement = $("#" + inputId);
+function validateCountryCode(inputId, i) {
+  const inputElement = $("#" + inputId + i);
   const countryCode = inputElement.val();
 
   // Remove non-numeric characters
@@ -7,31 +7,31 @@ function validateCountryCode(inputId) {
 
   if (numericCountryCode.length <= 5) {
     inputElement.val(numericCountryCode);
-    $("#" + inputId + "Error")
+    $("#" + inputId + "Error" + i)
       .css("color", "red")
       .text("");
     if (/^\d+$/.test(numericCountryCode)) {
-      $("#mobileLabel").addClass("required-label");
-      $("#countryCodeLabel").addClass("required-label");
+      $("#mobileLabel" + i).addClass("required-label");
+      $("#countryCodeLabel" + i).addClass("required-label");
     } else {
-      $("#countryCodeLabel").removeClass("required-label");
-      $("#mobileLabel").removeClass("required-label");
+      $("#countryCodeLabel" + i).removeClass("required-label");
+      $("#mobileLabel" + i).removeClass("required-label");
     }
   } else if (numericCountryCode.length === 0) {
-    $("#countryCodeLabel").removeClass("required-label");
-    $("#mobileLabel").removeClass("required-label");
-    $("#" + inputId + "Error").text("");
+    $("#countryCodeLabel" + i).removeClass("required-label");
+    $("#mobileLabel" + i).removeClass("required-label");
+    $("#" + inputId + "Error" + i).text("");
   } else {
-    $("#" + inputId + "Error")
+    $("#" + inputId + "Error" + i)
       .css("color", "red")
       .text("Country code must be between 2 and 5 digits.");
-    $("#countryCodeLabel").removeClass("required-label");
-    $("#mobileLabel").removeClass("required-label");
+    $("#countryCodeLabel" + i).removeClass("required-label");
+    $("#mobileLabel" + i).removeClass("required-label");
   }
 }
 
-function validateMobileNumber(inputId) {
-  const inputElement = $("#" + inputId);
+function validateMobileNumber(inputId, i) {
+  const inputElement = $("#" + inputId + i);
   const mobileNumber = inputElement.val();
 
   // Remove non-numeric characters
@@ -40,48 +40,97 @@ function validateMobileNumber(inputId) {
   if (numericMobileNumber.length <= 10) {
     inputElement.val(numericMobileNumber);
     if (numericMobileNumber.length < 10) {
-      $("#" + inputId + "Error")
+      $("#" + inputId + "Error" + i)
         .css("color", "red")
         .text("Mobile number must be at least 10 digits.");
     } else {
-      $("#" + inputId + "Error").text("");
+      $("#" + inputId + "Error" + i).text("");
     }
 
     if (/^\d+$/.test(numericMobileNumber)) {
-      $("#countryCodeLabel").addClass("required-label");
-      $("#mobileLabel").addClass("required-label");
+      $("#countryCodeLabel" + i).addClass("required-label");
+      $("#mobileLabel" + i).addClass("required-label");
     } else {
-      $("#countryCodeLabel").removeClass("required-label");
-      $("#mobileLabel").removeClass("required-label");
+      $("#countryCodeLabel" + i).removeClass("required-label");
+      $("#mobileLabel" + i).removeClass("required-label");
     }
   } else if (numericMobileNumber.length === 0) {
-    $("#countryCodeLabel").removeClass("required-label");
-    $("#mobileLabel").removeClass("required-label").html("Mobile Number:");
-    $("#" + inputId + "Error").text("");
+    $("#countryCodeLabel" + i).removeClass("required-label");
+    $("#mobileLabel" + i)
+      .removeClass("required-label")
+      .html("Mobile Number:");
+    $("#" + inputId + "Error" + i).text("");
   } else {
-    $("#" + inputId + "Error")
+    $("#" + inputId + "Error" + i)
       .css("color", "red")
       .text("Mobile number must be exactly 10 digits.");
-    $("#countryCodeLabel").removeClass("required-label");
-    $("#mobileLabel").removeClass("required-label");
+    $("#countryCodeLabel" + i).removeClass("required-label");
+    $("#mobileLabel" + i).removeClass("required-label");
+  }
+}
+function generateForms(count) {
+  const div = $("#formsDiv");
+  div.empty(); // Clear existing forms
+
+  const formFieldsHtml = function (i) {
+    return `
+      <label for="name${i}">Name:</label>
+      <input type="text" id="name${i}" name="name${i}" required />
+
+      <label for="email${i}">Email:</label>
+      <input type="email" id="email${i}" name="email${i}" required />
+
+      <label for="countryCode${i}" id="countryCodeLabel${i}">Country Code:</label>
+      <input
+        type="text"
+        id="countryCode${i}"
+        name="countryCode${i}"
+        oninput="validateCountryCode('countryCode', ${i})"
+        required
+        minlength="2"
+        maxlength="5"
+      />
+      <span id="countryCodeError${i}" class="error-message"></span>
+
+      <label for="mobile${i}" id="mobileLabel${i}">Mobile Number:</label>
+      <input
+        type="text"
+        id="mobile${i}"
+        oninput="validateMobileNumber('mobile', ${i})"
+        name="mobile${i}"
+        required
+        minlength="10"
+        maxlength="10"
+      />
+      <span id="mobileError${i}" class="error-message"></span>
+
+      <label for="dob${i}">Date of Birth:</label>
+      <input type="date" id="dob${i}" name="dob${i}" required />
+
+      <label>Gender:</label>
+      <input type="radio" name="gender${i}" value="male" required /> Male
+      <input type="radio" name="gender${i}" value="female" required /> Female
+
+      <label for="password${i}">Password:</label>
+      <input type="password" id="password${i}" name="password${i}" required />
+    `;
+  };
+
+  for (let i = 1; i <= count; i++) {
+    const formHtml = `
+      <form id="form${i}" class="styled-form">
+        <h2>Form ${i}</h2>
+        ${formFieldsHtml(i)}
+        <button type="submit">Submit</button>
+      </form>
+    `;
+    div.append(formHtml);
   }
 }
 
-// $("#countryCode").on("input", function () {
-//   if ($(this).val().length > 0) {
-//     validateCountryCode("countryCode");
-//   } else {
-//     $("#countryCodeLabel").removeClass("required-label");
-//     $("#mobileLabel").removeClass("required-label");
-//   }
-// });
-
-// $("#mobile").on("input", function () {
-//   if ($(this).val().length > 0) {
-//     validateMobileNumber("mobile");
-//   } else {
-//     $("#countryCodeLabel").removeClass("required-label");
-//     $("#mobileLabel").removeClass("required-label");
-//     $("#mobileError").text("");
-//   }
-// });
+$(document).ready(function () {
+  $("#generateFormsButton").click(function () {
+    const formCount = parseInt($("#formCountInput").val(), 10);
+    generateForms(formCount);
+  });
+});
